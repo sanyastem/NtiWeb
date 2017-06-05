a1e742068166f6215d8bf1dc30cba18b75c78bd3
ignatov-dmitry <ignatov.d43@gmail.com>
2017-06-05 23:31:02 +0300

    add

---
+++ ASUVP.Online.Web/Models/Claim/ClaimTemplate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASUVP.Online.Web.Models.Claim
+{
+    public class ClaimTemplate
+    {
+        public int id { get; set; }
+        public string guid { get; set; }
+
+    }
+}
